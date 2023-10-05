b696669e235d4c4dd40ffe209201ac1573e3e2e5
Vladimir Bychkov <vreboot@yandex.ru>
2023-10-05 19:35:49 +0900

    первый коммит проекта 05 окт 2023 г.

---
+++ config/db.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    // 'class' => 'yii\db\Connection',
+    // 'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    // 'username' => 'root',
+    // 'password' => '',
+    // 'charset' => 'utf8',
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=192.168.55.254;port=5432;dbname=forms',
+    'username' => 'forms',
+    'password' => 'forms',
+    'charset' => 'utf8',
+
+
+
+
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
